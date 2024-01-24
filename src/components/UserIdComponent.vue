a226e2fc497f8eadffa7b5ca4233f36bb966a2fa
Ioannis P. Christovasilis <Jesusbill@users.noreply.github.com>
2024-01-24 15:21:15 +0100

    fix

--- src/components/UserIdComponent.vue
+++ src/components/UserIdComponent.vue
@@ -7,7 +7,7 @@
 </template>
   
 <script>
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import { useUserStore } from '@/stores/user.js'; // Import your Pinia store
 import { storeToRefs } from 'pinia'
 
@@ -16,8 +16,6 @@ export default {
     setup() {
         
         const { userId } = storeToRefs(useUserStore())
-        // const userId = ref(null);
-        console.log(userId)
 
         // Read the user ID from the cookie during component mounting
         onMounted(() => {
@@ -27,7 +25,7 @@ export default {
                 ?.split('=')[1];
 
             // Set the userId ref with the value from the cookie
-            userId = cookieValue || null;
+            userId.value = cookieValue || null;
         });
 
         return {
